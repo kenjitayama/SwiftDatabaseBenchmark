b886ff1914daaad373ff89398990de8db0ebdef8
Kenji Tayama <kenji.tayama@gmail.com>
2016-03-14 19:26:58 +0900

    Initial Commit

---
+++ SwiftDatabaseBenchmark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDatabaseBenchmark
+//
+//  Created by Kenji Tayama on 2016/03/14.
+//  Copyright © 2016年 Kenji Tayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
